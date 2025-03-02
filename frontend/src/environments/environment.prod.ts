d60527ae0757efe98b28f01b6af2efe69107e80b
Dmitry Grinko <dm.grinko@gmail.com>
2025-03-02 11:27:33 -0800

    feat: update env vars

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://XXXX.execute-api.us-east-1.amazonaws.com/dev',
   webSocketUrl: 'wss://XXXX.execute-api.us-east-1.amazonaws.com/dev',
-  webSocketConnectionsUrl: 'https://XXXX.execute-api.us-east-1.amazonaws.com/dev/@connections'
+  webSocketConnectionsUrl: 'https://XXXX.execute-api.us-east-1.amazonaws.com/dev/@connections',
+  googleClientId: 'XXXX'
 };
