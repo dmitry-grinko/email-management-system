6ae5004961c32d79d7ff9a38bfe42f7e3aeb1167
Dmitry Grinko <dm.grinko@gmail.com>
2025-03-01 21:29:08 -0800

    feat: env vars for Angular

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './app/interceptors/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+
+    provideHttpClient(withInterceptors([authInterceptor ]), withFetch()),
   ]
 }).catch(err => console.error(err)); 
