12c4a15eca02b9789dbb191a72f915c631130c74
Dmitry Grinko <dm.grinko@gmail.com>
2025-03-02 20:28:07 -0800

    fix the env vars problem

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   apiUrl: 'https://XXXX.execute-api.us-east-1.amazonaws.com/dev',
   webSocketUrl: 'wss://XXXX.execute-api.us-east-1.amazonaws.com/dev',
   webSocketConnectionsUrl: 'https://XXXX.execute-api.us-east-1.amazonaws.com/dev/@connections',
-  googleClientId: 'XXXX.apps.googleusercontent.com'
+  googleClientId: 'XXXX.apps.googleusercontent.com', // Comma at the end of the line is required for the update-env.js script
 };
