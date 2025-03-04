47ab24dd8a0febcf5bd1cbec2bc4f2579c4e68e1
Dmitry Grinko <dm.grinko@gmail.com>
2025-03-03 16:44:22 -0800

    test

--- backend/lambdas/webhook/src/index.ts
+++ backend/lambdas/webhook/src/index.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('Webhook event:', JSON.stringify(event, null, 2));
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello, world!' + event.body }),
